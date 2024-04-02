220e49c14917a17f4bcd43651d4c8353dec4f1a8
Laurent Brusa <wrmultitudes@gmail.com>
2024-04-02 16:37:26 +0200

    forgot to close quotes

--- tests/part2_tests.cpp
+++ tests/part2_tests.cpp
@@ -6,11 +6,11 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 12:27:39 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/04/02 16:07:04 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/04/02 16:36:00 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "razorclam_tests.h
+#include "razorclam_tests.h"
 // add the necessary includes
 
 const char *all_tests()
